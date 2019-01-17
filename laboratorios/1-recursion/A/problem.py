fae9d16ee575f731233372a9a6077b16634be969
unknown <2128980@labinfo.is.escuelaing.edu.co>
2019-01-17 08:40:23 -0500

    ejercicio a

--- laboratorios/1-recursion/A/problem.py
+++ laboratorios/1-recursion/A/problem.py
@@ -2,16 +2,23 @@ import json
 
 
 # TODO Complete!!
-def reverse(text):
-    return text
+def reverse(text,textrev,cont):
+    if cont==len(text)+1:
+        return textrev
+    else:
+        textrev+=text[len(text)-cont]
+        return reverse(text,textrev,cont+1)
+        
+     
 
-
+textrev=""
+cont=1
 if __name__ == '__main__':
     with open('./data.json') as f:
         tests = json.load(f)
         for i, test in enumerate(tests):
             text = test["text"]
-            actual = reverse(text)
+            actual = reverse(text,textrev,cont)
             expected = test['result']
             assert actual == expected, f'Test {i} | expected: {expected}, actual: {actual}'
         print('OK!')
