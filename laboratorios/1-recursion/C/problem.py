638995a2dc19f58a206ef95cdea1fdb1372dba32
unknown <2128980@labinfo.is.escuelaing.edu.co>
2019-01-17 09:00:39 -0500

    ejercicio c

--- laboratorios/1-recursion/C/problem.py
+++ laboratorios/1-recursion/C/problem.py
@@ -2,16 +2,28 @@ import json
 
 
 # TODO Complete!
-def has_more_vowels(s):
-    return False
+def has_more_vowels(s,vocales):
+    numvoc=0
+    
+    for i in s:
+        for k in vocales:
+            if str.lower(k)==str.lower(i):
+                numvoc+=1
+    
+    if numvoc > len(s)-numvoc:
+        return True
+    else:
+        return False
+        
+    
 
-
+vocales=["a","e","i","o","u"]
 if __name__ == '__main__':
     with open('./data.json') as f:
         tests = json.load(f)
         for i, test in enumerate(tests):
             s = test["s"]
-            actual = has_more_vowels(s)
+            actual = has_more_vowels(s,vocales)
             expected = test['result']
             assert actual == expected, f'Test {i} | expected: {expected}, actual: {actual}'
         print('OK!')
